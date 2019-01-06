b562cb7e4885b8ec9cb5192b0b067142b517fe89
niesssiobhan <niesssiobhan@live.com>
2019-01-05 22:39:07 -0800

    comepletd the lab

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 'use strict';
 
 const events = require('./lib/events.js');
-const files = require('./lib/files.js');
+const fs = require('fs');
 const logger = require('./lib/logger.js');
 
-let file = process.argv.slice(2).shift();
-if(file) {
-  files.alterFile(file);
+let myFile = process.argv.slice(2).shift();
+
+function alterFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) events.emit('error', {file: myFile});
+      resolve(data);
+    });
+  });
 }
+
+alterFile(myFile).then((text) => {
+  return text.toString().toUpperCase();
+}).then ((text) => {
+  fs.writeFile(myFile, Buffer.from(text), (err, data) => {
+    if(err) events.emit('error', {file: myFile});
+    events.emit('saved', {file: myFile});
+  });
+});
+
+alterFile(myFile);
